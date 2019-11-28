9eb0169d69643c17cb95fce544a2a4dba4bd6987
frontendsmallAnt <1041832785@qq.com>
2019-11-28 08:56:12 +0800

    wbpack-test

---
+++ webpack.prod.js
@@ -0,0 +1,10 @@
+const commonConfig = require('./webpack.common')
+const merge = require('webpack-merge');
+
+const prodConfig={
+    mode: 'production',
+    devtool: 'cheap-module-source-map',
+}
+
+
+module.exports=merge(commonConfig,prodConfig)
